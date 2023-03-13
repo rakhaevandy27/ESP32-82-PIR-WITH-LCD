61f0988e2a78c6ab90c01320a339f6025672735b
MUHAMMAD RAKHA EVANDY CHRISNA <61354650+rakhaevandy27@users.noreply.github.com>
2023-03-13 22:41:39 +0700

    Update and rename PIR SENSOR CM AND INCH + LCD I2C.ino to index.ino

--- index.ino
+++ index.ino
@@ -1,32 +1,32 @@
 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal_I2C.h> //inlude library for LCD I2C
+LiquidCrystal_I2C lcd(0x27, 16, 2); //initial,addres,hpx,wpx
 
 
-const int trigPin = 9;
-const int echoPin = 8;
+const int trigPin = 9; //trigger pin
+const int echoPin = 8; //echo pin
 
 //define sound velocity in cm/uS
 #define SOUND_VELOCITY 0.034
 #define CM_TO_INCH 0.393701
-#define ledr 3
-#define ledg 2
-#define relay 4
+#define ledr 3 //define led red on GPIO 3
+#define ledg 2 //define led green on GPIO 2
+#define relay 4 //define relay on GPIO 4 (OPTIONAL)
 
 
-long duration;
-float distanceCm;
-float distanceInch;
+long duration; //variable for time
+float distanceCm; //variable for distance on CM(Centi Meter)
+float distanceInch; //variable for distance on INCH
 
 void setup() {
   Serial.begin(115200); // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode(ledr,OUTPUT);
-  pinMode(ledg,OUTPUT);
-  pinMode(relay,OUTPUT);
-  lcd.begin();
-  lcd.backlight();
+  pinMode(ledr,OUTPUT); // Sets the red led as an Output
+  pinMode(ledg,OUTPUT); // Sets the green led as an Output
+  pinMode(relay,OUTPUT); // Sets the relay as an Output
+  lcd.begin(); //lcd begin or starting the module //sometimes other library version use lcd.begin(16,2)
+  lcd.backlight(); //turn on lcd led backlight
 }
 
 void loop() {
@@ -62,6 +62,7 @@ void loop() {
   lcd.print("(inch) :  ");
   lcd.print(distanceInch);
 
+  //logic function for executing 
   if(distanceCm < 30 || distanceCm <= 50){
     digitalWrite(relay,LOW);
     delay(5);
